07525c3d482080dcf9088ff0cfe0a3f6f917f913
kaczifant <35376563+kaczifant@users.noreply.github.com>
2019-10-15 17:04:20 +0200

    Add files via upload

---
+++ Language Proficiency/whats_your_name.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/whats-your-name/problem
+
+'''
+You are given the firstname and lastname of a person on two different lines. 
+Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+'''
+
+
+# Write your code below
+
+def print_full_name(a, b):
+    print(f"Hello {a} {b}! You just delved into python.")
+
