b0a5595fd853c7a3175f5954fda5d7cc2395c945
kaczifant <35376563+kaczifant@users.noreply.github.com>
2019-10-15 16:51:53 +0200

    Add files via upload

---
+++ finding_the_percentage.py
@@ -0,0 +1,15 @@
+# https://www.hackerrank.com/challenges/finding-the-percentage/problem
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+
+	# Write your code here
+	
+    average = sum(student_marks[query_name])/3
+    print("%.2f" % average)
